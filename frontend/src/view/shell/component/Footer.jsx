0e86e8eeb62cd99593726ddfda0c91e249de0226
Kevin Cordano <kmcordano@gmail.com>
2024-08-16 14:23:29 -0500

    Implemented time clock

--- frontend/src/view/shell/component/Footer.jsx
+++ frontend/src/view/shell/component/Footer.jsx
@@ -1,15 +1,29 @@
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 import '../style/Footer.css';
 
+// const currentDate = new Date().toLocaleDateString();
+// const currentTime = new Date().toLocaleTimeString();
+
 const Footer = ({ user }) => {
+   const [currentDate, setCurrentDate] = useState(new Date());
+
+   useEffect(() => {
+      const intervalId = setInterval(() => {
+         setCurrentDate(new Date());
+      }, 1000);
+      
+      return () => clearInterval(intervalId);
+   }, []);
+
    return (
       <div className="Footer">
          <div className="Footer-user">
             { user ? <h2>{user.firstName} {user.lastName} - {user.employeeNumber}</h2> : <></> }
          </div>
          <div className="Footer-timestamp">
-            <h2>Time - Date</h2>
+            <h2>{currentDate.toLocaleString()}</h2>
          </div>
       </div>
    )
